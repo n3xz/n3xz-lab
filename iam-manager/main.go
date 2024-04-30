e199f6b109ae2cfbc9f953e1e27d1a4d82c396e0
The 0x <0xvmx@proton.me>
2024-04-30 17:30:42 -0500

    moar test

--- iam-manager/main.go
+++ iam-manager/main.go
@@ -1,20 +1,57 @@
 package main
 
 import (
-	"github.com/pulumi/pulumi-aws/sdk/v6/go/aws/s3"
+	"encoding/json"
+	"go/ast"
+	"go/parser"
+	"go/token"
+	"log"
+
+	"github.com/pulumi/pulumi-aws/sdk/go/aws/iam"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
 func main() {
-	pulumi.Run(func(ctx *pulumi.Context) error {
-		// Create an AWS resource (S3 Bucket)
-		bucket, err := s3.NewBucket(ctx, "my-bucket", nil)
+	pulumi.Run(func(ctx *pulumi.Context) error{
+		fset := token.NewFileSet()
+		node, err := parser.ParseFile(fset, "../static-site/main.go", nil, 0)
 		if err != nil {
-			return err
+			log.Fatal(err)
 		}
+		
+		mapOfAwsCalls := make(map[string]string)
+		ast.Inspect(node, func(n ast.Node) bool {
+			switch x := n.(type) {
+			case *ast.SelectorExpr:
+				if x.X.(*ast.Ident).Name == "s3" {
+					log.Println("Found pulumi code that uses the aws sdk")
+					mapOfAwsCalls[x.Sel.Name] = x.X.(*ast.Ident).Name
 
-		// Export the name of the bucket
-		ctx.Export("bucketName", bucket.ID())
+					tmpJSON0, err := json.Marshal(map[string]interface{}{
+						"Version": "2012-10-17",
+						"Statement": []map[string]interface{}{
+							map[string]interface{}{
+								"Action": []string{
+									"ec2:Describe*",
+								},
+								"Effect":   "Allow",
+								"Resource": "*",
+							},
+						},
+					})
+					if err != nil {
+						log.Fatal(err)
+					}
+
+					_, err = iam.NewRolePolicy(ctx, "s3Policy", &iam.RolePolicyArgs{
+						Policy: pulumi.String(tmpJSON0),
+						Role:   pulumi.Any(aws_iam_role.Example.Arn),
+						
+				}
+			}
+			
+			return true
+		})
 		return nil
 	})
 }
