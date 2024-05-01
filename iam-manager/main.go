9e081fcb4b171e0258af199e10a59bc0780617e2
The 0x <0xvmx@proton.me>
2024-05-01 00:14:22 -0500

    more awesome

--- iam-manager/main.go
+++ iam-manager/main.go
@@ -1,35 +1,54 @@
 package main
 
 import (
+	"github.com/pulumi/pulumi-aws/sdk/v6/go/aws"
+	"github.com/pulumi/pulumi-aws/sdk/v6/go/aws/iam"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 	"github.com/pulumiverse/pulumi-doppler/sdk/go/doppler"
 )
 
+func credGrabber(ctx *pulumi.Context) map[string]string  {
+
+	project := string(pulumi.String("iam-manager"))
+	config := string(pulumi.String("dev"))
+
+	credentials, err := doppler.GetSecrets(ctx, &doppler.GetSecretsArgs{
+		Project: &project,
+		Config:  &config,
+	})
+	if err != nil {
+		return nil
+	}
+
+	return credentials.Map
+}
 
 
 func main() {
-
 	pulumi.Run(func(ctx *pulumi.Context) error {
 
-		project := string(pulumi.String("iam-manager"))
-		config := string(pulumi.String("dev"))
+		creds := credGrabber(ctx)
+		if creds == nil {
+			return nil
+		}
 
-		credentials, err := doppler.GetSecrets(ctx, &doppler.GetSecretsArgs{
-			Project: &project,
-			Config:  &config,
+		provider, err := aws.NewProvider(ctx, "aws", &aws.ProviderArgs{
+			Region: pulumi.String("us-west-2"),
+			AccessKey: pulumi.String(creds["AWS_ACCESS_KEY_ID"]),
+			SecretKey: pulumi.String(creds["AWS_SECRET_ACCESS_KEY"]),
 		})
 		if err != nil {
 			return err
 		}
 
-		for key, value := range credentials.Map {
-			if key != "AWS_SECRET_ACCESS_KEY" {
-				ctx.Export(key, pulumi.String(value))
-			} else {
-				ctx.Export(key, pulumi.String("********"))
-			}
+		user, errp := iam.NewUser(ctx, "my-user", &iam.UserArgs{
+			Path: pulumi.String("/"),
+		}, pulumi.Provider(provider))
+		if errp != nil {
+			return errp
 		}
 
+		ctx.Export("user-name", user.Name)
 		return nil
 	})
 }
